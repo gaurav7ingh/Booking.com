307b497a61d1b1af4b4da97a8422e321d948dad0
Raj <officialraj480@gmail.com>
2022-07-21 20:40:37 +0530

    ipdated

---
+++ flight.js
@@ -0,0 +1,10 @@
+let data=[
+    {img:"https://q-xx.bstatic.com/xdata/images/city/square210/620027.jpg?k=3e415bb694a1a0145529dad3242573d0d52364bc57bae824b5990bf9c2fabc04&o=",name:"Kolkata to Bangkok",wdate:"Jul 24 - Jul 31 ",wh:"Round trip"},
+    {img:"https://q-xx.bstatic.com/xdata/images/city/square210/674773.jpg?k=f65fb36a6b12a3f09a07232ef7946deb46871c0d5a308d3e16ff127d61233f41&o=",name:"Kolkata to Male City",wdate:"Jul 26 - Aug 2 ",wh:"Round trip"},
+    {img:"https://q-xx.bstatic.com/xdata/images/city/square210/619923.jpg?k=4fb13225390240a51ee5aa1d76318d03dc0de8a046ddc06e4598f17b287bdcc9&o=",name:"Kolkata to Dubai",wdate:"Jul 21 - Jul 28  ",wh:"Round trip"},
+    {img:"https://q-xx.bstatic.com/xdata/images/city/square210/613087.jpg?k=68ce65e52a527819c35c13c3d0e8a925263a71aab15a89577b4083c021855481&o=",name:"Kolkata to Paris",wdate:"Jul 26 - Aug 2 ",wh:"Round trip"},
+
+];
+data.forEach(e => {
+   
+});
