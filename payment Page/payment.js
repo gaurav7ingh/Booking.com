9d28d0ce531b738bcc17b27e913283d8369e06ae
gaurav singh <gauravsingh99626@gmail.com>
2022-07-23 12:39:31 +0530

    meage all

--- payment Page/payment.js
+++ payment Page/payment.js
@@ -1,2 +1,11 @@
 let data = JSON.parse(localStorage.getItem("_data_"))
 
+let promobtn = document.querySelector("#promoDiv>button")
+promobtn.addEventListener("click", () => {
+  let promoCode = document.querySelector("#promoDiv>input")
+  if (promoCode.value == "masai30") {
+    alert("You Will get 30% back on CheckIn")
+  }else{
+    alert("Enter Valid Promo")
+  }
+})
